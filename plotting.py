7152a24e2056c51f5335ca616e106d3db351f49e
ReallyRad <arthur.pointeau@gmail.com>
2017-09-22 14:57:24 +0200

    added grouping per neighborhood

--- plotting.py
+++ plotting.py
@@ -9,24 +9,27 @@ year_2016 = open('2016.csv')
 year_files = [year_2013, year_2014, year_2015, year_2016]
 
 nationalities = ['Mexic','Xina','Argentina','Filipines']
-neighborhood = ['1. el Raval']
-offset = 6
+colors = ["red","purple","green", "brown"]
+neighborhoods = ['1. el Raval','3. la Barceloneta','15. Hostafrancs','18. Sants']
+offset = .5
 
-#for each nationality
-for i,n in enumerate(nationalities):
-    pop_per_nat = []
+for ind_nei, nei in enumerate(neighborhoods):
+    #for each nationality
+    for i,n in enumerate(nationalities):
+        pop_per_nat = []
 
-    #get the data for each year
-    for year in range(2013, 2017):
-        with open(str(year)+".csv") as f:
-            csv_reader = csv.DictReader(f)
-            for row in csv_reader:
+        #get the data for each year
+        for year in range(2013, 2017):
+            with open(str(year)+".csv") as f:
+                csv_reader = csv.DictReader(f)
+                for row in csv_reader:
 
-                #find the neighborhood we are working on
-                if row['Barris'] == neighborhood[0]:
+                    #find the neighborhood we are working on
+                    if row['Barris'] == neighborhoods[ind_nei]:
 
-                    #getting the population for that year in that neighborhood
-                    pop_per_nat.append(row[n])
+                        #getting the population for that year in that neighborhood
+                        pop_per_nat.append(row[n])
+        plt.subplot(2,2,ind_nei+1)
+        plt.bar([float(x)+ i*offset for x in range(2013,2017)], pop_per_nat, color=colors[i])
 
-    plt.bar([x+ i*offset for x in range(2013,2017)], pop_per_nat)
-    plt.figure(i)
+plt.show()
